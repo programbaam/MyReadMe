ed0cec19b337ce97b0d6feade23cd0fe69f7e233
이범호 <96612703+programbaam@users.noreply.github.com>
2026-05-26 21:09:57 +0900

    second commit
    2번째 커밋

---
+++ Source/GitTest/GitActor.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GitActor.h"
+
+// Sets default values
+AGitActor::AGitActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGitActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGitActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
