8a527722c6733028ed6658e994bcc1e2b3e7b5e0
이범호 <96612703+programbaam@users.noreply.github.com>
2026-05-26 21:23:22 +0900

    Update GitActor.cpp

--- Source/GitTest/GitActor.cpp
+++ Source/GitTest/GitActor.cpp
@@ -14,7 +14,7 @@ AGitActor::AGitActor()
 void AGitActor::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("Test"));
+	UE_LOG(LogTemp, Warning, TEXT("Test456"));
 }
 
 // Called every frame
